6c86a6ad90d681f33b578efc2bbe52876cded3fc
TeraWattHour <norman_wiese@icloud.com>
2023-10-02 18:05:41 +0200

    fix: no need for pointer check

--- src/curl_helpers.c
+++ src/curl_helpers.c
@@ -8,25 +8,20 @@ typedef struct curl_response {
 } CurlResponse;
 
 CurlResponse init_curl_response() {
-    CurlResponse data = { .content = "", .size = 0 };
+    CurlResponse data = { .content = palloc(0), .size = 0 };
     return data;
 }
 
 static size_t curl_generic_callback(void *data, size_t size, size_t nmemb, CurlResponse *mem) {
     size_t sz = size * nmemb;
 
-    if(mem->content == NULL || mem->size <= 0) {
-        mem->content = palloc(sz + 1);
-    } else {
-        mem->content = repalloc(mem->content, mem->size + sz + 1);
-    }
-
+    mem->content = repalloc(mem->content, mem->size + sz + 1);
     if(mem->content == NULL)
         return 0;
 
     memcpy(&(mem->content[mem->size]), data, sz);
     mem->size += sz;
-    mem->content[mem->size] = 0;
+    mem->content[mem->size] = '\0';
 
     return sz;
 }
