8eee480db1d0a544859d85c3e3c2a1c1ce38d03b
TeraWattHour <norman_wiese@icloud.com>
2023-10-02 15:04:05 +0200

    changes: reformatted FindPostgreSQL.cmake

--- cmake/FindPostgreSQL.cmake
+++ cmake/FindPostgreSQL.cmake
@@ -48,119 +48,118 @@
 # Define additional search paths for root directories.
 set(PostgreSQL_ROOT_DIRECTORIES ENV PGROOT ENV PGPATH ${PostgreSQL_ROOT})
 
+message(STATUS "${PostgreSQL_ROOT_DIRECTORIES}")
+
 find_program(
-        PG_CONFIG pg_config
-        PATHS ${PostgreSQL_ROOT_DIRECTORIES}
-        PATH_SUFFIXES bin
+    PG_CONFIG pg_config
+    PATHS ${PostgreSQL_ROOT_DIRECTORIES}
+    PATH_SUFFIXES bin
 )
 
 if (NOT PG_CONFIG)
     message(FATAL_ERROR "Could not find pg_config")
-else ()
-    set(PostgreSQL_FOUND TRUE)
 endif ()
 
 message(STATUS "Found pg_config as ${PG_CONFIG}")
 
-if (PostgreSQL_FOUND)
-    macro(PG_CONFIG VAR OPT)
-        execute_process(
-                COMMAND ${PG_CONFIG} ${OPT}
-                OUTPUT_VARIABLE ${VAR}
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-    endmacro()
-
-    pg_config(_pg_bindir --bindir)
-    pg_config(_pg_includedir --includedir)
-    pg_config(_pg_pkgincludedir --pkgincludedir)
-    pg_config(_pg_sharedir --sharedir)
-    pg_config(_pg_includedir_server --includedir-server)
-    pg_config(_pg_libs --libs)
-    pg_config(_pg_ldflags --ldflags)
-    pg_config(_pg_ldflags_sl --ldflags_sl)
-    pg_config(_pg_ldflags_ex --ldflags_ex)
-    pg_config(_pg_pkglibdir --pkglibdir)
-    pg_config(_pg_libdir --libdir)
-    pg_config(_pg_version --version)
-
-    separate_arguments(_pg_ldflags)
-    separate_arguments(_pg_ldflags_sl)
-    separate_arguments(_pg_ldflags_ex)
-
-    set(_server_lib_dirs ${_pg_libdir} ${_pg_pkglibdir})
-    set(_server_inc_dirs ${_pg_pkgincludedir} ${_pg_includedir_server})
-    string(REPLACE ";" " " _shared_link_options
-            "${_pg_ldflags};${_pg_ldflags_sl}")
-    set(_link_options ${_pg_ldflags})
-    if (_pg_ldflags_ex)
-        list(APPEND _link_options ${_pg_ldflags_ex})
-    endif ()
-
-    set(PostgreSQL_INCLUDE_DIRS
-            "${_pg_includedir}"
-            CACHE PATH
-            "Top-level directory containing the PostgreSQL include directories."
+macro(pg_config VAR OPT)
+    execute_process(
+        COMMAND ${PG_CONFIG} ${OPT}
+        OUTPUT_VARIABLE ${VAR}
+        OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    set(PostgreSQL_EXTENSION_DIR
-            "${_pg_sharedir}/extension"
-            CACHE PATH "Directory containing extension SQL and control files")
-    set(PostgreSQL_SERVER_INCLUDE_DIRS
-            "${_server_inc_dirs}"
-            CACHE PATH "PostgreSQL include directories for server include files.")
-    set(PostgreSQL_LIBRARY_DIRS
-            "${_pg_libdir}"
-            CACHE PATH "library directory for PostgreSQL")
-    set(PostgreSQL_LIBRARIES
-            "${_pg_libs}"
-            CACHE PATH "Libraries for PostgreSQL")
-    set(PostgreSQL_SHARED_LINK_OPTIONS
-            "${_shared_link_options}"
-            CACHE STRING "PostgreSQL linker options for shared libraries.")
-    set(PostgreSQL_LINK_OPTIONS
-            "${_pg_ldflags},${_pg_ldflags_ex}"
-            CACHE STRING "PostgreSQL linker options for executables.")
-    set(PostgreSQL_SERVER_LIBRARY_DIRS
-            "${_server_lib_dirs}"
-            CACHE PATH "PostgreSQL server library directories.")
-    set(PostgreSQL_VERSION_STRING
-            "${_pg_version}"
-            CACHE STRING "PostgreSQL version string")
-    set(PostgreSQL_PACKAGE_LIBRARY_DIR
-            "${_pg_pkglibdir}"
-            CACHE STRING "PostgreSQL package library directory")
+endmacro()
 
-    find_program(
-            PG_BINARY postgres
-            PATHS ${PostgreSQL_ROOT_DIRECTORIES}
-            HINTS ${_pg_bindir}
-            PATH_SUFFIXES bin
-    )
+pg_config(_pg_bindir --bindir)
+pg_config(_pg_includedir --includedir)
+pg_config(_pg_pkgincludedir --pkgincludedir)
+pg_config(_pg_sharedir --sharedir)
+pg_config(_pg_includedir_server --includedir-server)
+pg_config(_pg_libs --libs)
+pg_config(_pg_ldflags --ldflags)
+pg_config(_pg_ldflags_sl --ldflags_sl)
+pg_config(_pg_ldflags_ex --ldflags_ex)
+pg_config(_pg_pkglibdir --pkglibdir)
+pg_config(_pg_libdir --libdir)
+pg_config(_pg_version --version)
 
-    if (NOT PG_BINARY)
-        message(FATAL_ERROR "Could not find postgres binary")
-    endif ()
+separate_arguments(_pg_ldflags)
+separate_arguments(_pg_ldflags_sl)
+separate_arguments(_pg_ldflags_ex)
 
-    message(STATUS "Found postgres binary at ${PG_BINARY}")
-
-    find_program(PG_REGRESS pg_regress HINT
-            ${PostgreSQL_PACKAGE_LIBRARY_DIR}/pgxs/src/test/regress)
-    if (NOT PG_REGRESS)
-        message(STATUS "Could not find pg_regress, tests not executed")
-    endif ()
-
-    message(STATUS "PostgreSQL version ${PostgreSQL_VERSION_STRING} found")
-    message(
-            STATUS
-            "PostgreSQL package library directory: ${PostgreSQL_PACKAGE_LIBRARY_DIR}"
-    )
-    message(STATUS "PostgreSQL libraries: ${PostgreSQL_LIBRARIES}")
-    message(STATUS "PostgreSQL extension directory: ${PostgreSQL_EXTENSION_DIR}")
-    message(STATUS "PostgreSQL linker options: ${PostgreSQL_LINK_OPTIONS}")
-    message(
-            STATUS "PostgreSQL shared linker options: ${PostgreSQL_SHARED_LINK_OPTIONS}"
-    )
+set(_server_lib_dirs ${_pg_libdir} ${_pg_pkglibdir})
+set(_server_inc_dirs ${_pg_pkgincludedir} ${_pg_includedir_server})
+string(REPLACE ";" " " _shared_link_options "${_pg_ldflags};${_pg_ldflags_sl}")
+set(_link_options ${_pg_ldflags})
+if (_pg_ldflags_ex)
+    list(APPEND _link_options ${_pg_ldflags_ex})
 endif ()
+
+set(PostgreSQL_INCLUDE_DIRS
+    "${_pg_includedir}"
+    CACHE PATH "Top-level directory containing the PostgreSQL include directories."
+)
+set(PostgreSQL_EXTENSION_DIR
+    "${_pg_sharedir}/extension"
+    CACHE PATH "Directory containing extension SQL and control files"
+)
+set(PostgreSQL_SERVER_INCLUDE_DIRS
+    "${_server_inc_dirs}"
+    CACHE PATH "PostgreSQL include directories for server include files."
+)
+set(PostgreSQL_LIBRARY_DIRS
+    "${_pg_libdir}"
+    CACHE PATH "library directory for PostgreSQL"
+)
+set(PostgreSQL_LIBRARIES
+    "${_pg_libs}"
+    CACHE PATH "Libraries for PostgreSQL"
+)
+set(PostgreSQL_SHARED_LINK_OPTIONS
+    "${_shared_link_options}"
+    CACHE STRING "PostgreSQL linker options for shared libraries."
+)
+set(PostgreSQL_LINK_OPTIONS
+    "${_pg_ldflags},${_pg_ldflags_ex}"
+    CACHE STRING "PostgreSQL linker options for executables."
+)
+set(PostgreSQL_SERVER_LIBRARY_DIRS
+    "${_server_lib_dirs}"
+    CACHE PATH "PostgreSQL server library directories."
+)
+set(PostgreSQL_VERSION_STRING
+    "${_pg_version}"
+    CACHE STRING "PostgreSQL version string"
+)
+set(PostgreSQL_PACKAGE_LIBRARY_DIR
+    "${_pg_pkglibdir}"
+    CACHE STRING "PostgreSQL package library directory"
+)
+
+find_program(
+    PG_BINARY postgres
+    PATHS ${PostgreSQL_ROOT_DIRECTORIES}
+    HINTS ${_pg_bindir}
+    PATH_SUFFIXES bin
+)
+
+if (NOT PG_BINARY)
+    message(FATAL_ERROR "Could not find postgres binary")
+endif ()
+
+message(STATUS "Found postgres binary at ${PG_BINARY}")
+
+find_program(PG_REGRESS pg_regress HINT ${PostgreSQL_PACKAGE_LIBRARY_DIR}/pgxs/src/test/regress)
+if (NOT PG_REGRESS)
+    message(STATUS "Could not find pg_regress, tests not executed")
+endif ()
+
+message(STATUS "PostgreSQL version ${PostgreSQL_VERSION_STRING} found")
+message(STATUS "PostgreSQL package library directory: ${PostgreSQL_PACKAGE_LIBRARY_DIR}")
+message(STATUS "PostgreSQL libraries: ${PostgreSQL_LIBRARIES}")
+message(STATUS "PostgreSQL extension directory: ${PostgreSQL_EXTENSION_DIR}")
+message(STATUS "PostgreSQL linker options: ${PostgreSQL_LINK_OPTIONS}")
+message(STATUS "PostgreSQL shared linker options: ${PostgreSQL_SHARED_LINK_OPTIONS}")
 
 # add_postgresql_extension(NAME ...)
 #
@@ -209,30 +208,31 @@ function(add_postgresql_extension NAME)
         configure_file(${_template} ${_script} @ONLY)
         list(APPEND _script_files ${CMAKE_CURRENT_BINARY_DIR}/${_script})
         message(
-                STATUS "Building script file ${_script} from template file ${_template}"
+            STATUS "Building script file ${_script} from template file ${_template}"
         )
     endforeach ()
 
     set_target_properties(
-            ${NAME}
-            PROPERTIES PREFIX ""
-            LINK_FLAGS "${_link_flags}"
-            POSITION_INDEPENDENT_CODE ON)
+        ${NAME}
+        PROPERTIES PREFIX ""
+        LINK_FLAGS "${_link_flags}"
+        POSITION_INDEPENDENT_CODE ON
+    )
 
     target_include_directories(
-            ${NAME}
-            PRIVATE ${PostgreSQL_SERVER_INCLUDE_DIRS}
-            PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+        ${NAME}
+        PRIVATE ${PostgreSQL_SERVER_INCLUDE_DIRS}
+        PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}
+    )
 
     # Generate control file at build time (which is when GENERATE evaluate the
     # contents). We do not know the target file name until then.
     set(_control_file "${CMAKE_CURRENT_BINARY_DIR}/${NAME}.control")
     file(
-            GENERATE
-            OUTPUT ${_control_file}
-            CONTENT
-            "# This file is generated content from add_postgresql_extension.
-# No point in modifying it, it will be overwritten anyway.
+        GENERATE
+        OUTPUT ${_control_file}
+        CONTENT
+        "# This file is generated by add_postgresql_extension.
 
 # Default version, always set
 default_version = '${_ext_VERSION}'
@@ -253,8 +253,10 @@ $<$<NOT:$<BOOL:${_ext_REQUIRES}>>:#>requires = '$<JOIN:${_ext_REQUIRES},$<COMMA>
 ")
 
     install(TARGETS ${NAME} LIBRARY DESTINATION ${PostgreSQL_PACKAGE_LIBRARY_DIR})
-    install(FILES ${_control_file} ${_script_files}
-            DESTINATION ${PostgreSQL_EXTENSION_DIR})
+    install(
+        FILES ${_control_file} ${_script_files}
+        DESTINATION ${PostgreSQL_EXTENSION_DIR}
+    )
 
     if (_ext_REGRESS)
         foreach (_test ${_ext_REGRESS})
@@ -271,26 +273,28 @@ $<$<NOT:$<BOOL:${_ext_REQUIRES}>>:#>requires = '$<JOIN:${_ext_REQUIRES},$<COMMA>
 
         if (PG_REGRESS)
             add_test(
-                    NAME ${NAME}
-                    COMMAND
-                    ${PG_REGRESS} --temp-instance=${CMAKE_BINARY_DIR}/tmp_check
-                    --inputdir=${CMAKE_CURRENT_SOURCE_DIR}
-                    --outputdir=${CMAKE_CURRENT_BINARY_DIR} --load-extension=${NAME}
-                    ${_ext_REGRESS})
+                NAME ${NAME}
+                COMMAND
+                ${PG_REGRESS} --temp-instance=${CMAKE_BINARY_DIR}/tmp_check
+                --inputdir=${CMAKE_CURRENT_SOURCE_DIR}
+                --outputdir=${CMAKE_CURRENT_BINARY_DIR} --load-extension=${NAME}
+                ${_ext_REGRESS}
+            )
         endif ()
 
         add_custom_target(
-                ${NAME}_update_results
-                COMMAND
-                ${CMAKE_COMMAND} -E copy_if_different
-                ${CMAKE_CURRENT_BINARY_DIR}/results/*.out
-                ${CMAKE_CURRENT_SOURCE_DIR}/expected)
+            ${NAME}_update_results
+            COMMAND
+            ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_CURRENT_BINARY_DIR}/results/*.out
+            ${CMAKE_CURRENT_SOURCE_DIR}/expected
+        )
     endif ()
 endfunction()
 
 if (PG_REGRESS)
-    # We add a custom target to get output when there is a failure.
     add_custom_target(
-            test_verbose COMMAND ${CMAKE_CTEST_COMMAND} --force-new-ctest-process
-            --verbose --output-on-failure)
+        test_verbose COMMAND ${CMAKE_CTEST_COMMAND} --force-new-ctest-process
+        --verbose --output-on-failure
+    )
 endif ()
